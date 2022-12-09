8e97a79986234cbfff3fd8bfc04b6ca9ac629967
Evgenii Riabov <eryabov@inpromconsult.ru>
2022-12-09 15:53:36 +0300

    Moved to Java17 (to make code compatible to jdt.ls 1.18)

--- dg.jdt.ls.decompiler.fernflower.tests/src/dg/jdt/ls/decompiler/fernflower/FernflowerDecompilerTest.java
+++ dg.jdt.ls.decompiler.fernflower.tests/src/dg/jdt/ls/decompiler/fernflower/FernflowerDecompilerTest.java
@@ -32,9 +32,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class FernflowerDecompilerTest {
 
 	public static String HELLO_CLASS_PATH = "testclasses/HelloWorld.class";
--- dg.jdt.ls.decompiler.procyon.tests/src/dg/jdt/ls/decompiler/procyon/ProcyonDecompilerTest.java
+++ dg.jdt.ls.decompiler.procyon.tests/src/dg/jdt/ls/decompiler/procyon/ProcyonDecompilerTest.java
@@ -34,9 +34,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class ProcyonDecompilerTest {
 
 	public static String HELLO_CLASS_PATH = "testclasses/HelloWorld.class";
